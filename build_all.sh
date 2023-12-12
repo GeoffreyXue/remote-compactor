64debf2778fd2fa0b030e4d79bfdb7220137feea
GeoffreyXue <xuegeoff@gmail.com>
2023-12-12 01:02:20 -0600

    Adjust prefix paths

--- build_all.sh
+++ build_all.sh
@@ -2,7 +2,7 @@ git clone --depth 1 https://github.com/GeoffreyXue/metaarrow arrow
 cd arrow/cpp
 mkdir build
 cd build
-cmake .. -DARROW_PARQUET=ON -DARROW_JSON=ON -DARROW_BUILD_STATIC=OFF -DARROW_BUILD_SHARED=ON -GNinja
+cmake .. -DCMAKE_PREFIX_PATH=/usr/local/ -DCMAKE_INSTALL_PREFIX=/usr/local/ -DARROW_PARQUET=ON -DARROW_JSON=ON -DARROW_BUILD_STATIC=OFF -DARROW_BUILD_SHARED=ON -GNinja
 ninja -j 8
 ninja install
 cd ../../..
@@ -11,7 +11,7 @@ git clone -b compactor --depth 1 https://github.com/GeoffreyXue/metarocks rocksd
 cd rocksdb
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DROCKSDB_BUILD_SHARED=OFF -DWITH_TESTS=OFF -DWITH_ALL_TESTS=OFF -DWITH_TOOLS=OFF -GNinja
+cmake .. -DCMAKE_PREFIX_PATH=/usr/local/ -DCMAKE_BUILD_TYPE=Debug -DROCKSDB_BUILD_SHARED=OFF -DWITH_TESTS=OFF -DWITH_ALL_TESTS=OFF -DWITH_TOOLS=OFF -GNinja
 ninja -j 8
 cd ../..
 
@@ -19,11 +19,11 @@ git clone --recurse-submodules --depth 1 https://github.com/aws/aws-sdk-cpp
 cd aws-sdk-cpp/
 mkdir sdk_build
 cd sdk_build/
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=/usr/local/ -DCMAKE_INSTALL_PREFIX=/usr/local/  -DBUILD_ONLY="sqs"
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=/usr/local/ -DCMAKE_INSTALL_PREFIX=/usr/local/ -DBUILD_ONLY="sqs"
 make -j 8
 make install
-
 cd ../..
+
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1
