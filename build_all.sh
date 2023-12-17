1540c769b937d7739bd32e4acec945cca8cb0d12
Geoffrey Xue <xuegeoff@gmail.com>
2023-12-17 23:53:19 +0000

    Change build setup to build release

--- build_all.sh
+++ build_all.sh
@@ -11,7 +11,7 @@ git clone -b compactor --depth 1 https://github.com/GeoffreyXue/metarocks rocksd
 cd rocksdb
 mkdir build
 cd build
-cmake .. -DCMAKE_PREFIX_PATH=/usr/local/ -DCMAKE_BUILD_TYPE=Debug -DROCKSDB_BUILD_SHARED=OFF -DWITH_TESTS=OFF -DWITH_ALL_TESTS=OFF -DWITH_TOOLS=OFF -DWITH_BENCHMARK_TOOLS=OFF -DWITH_CORE_TOOLS=OFF -DWITH_TRACE_TOOLS=OFF -GNinja
+cmake .. -DCMAKE_PREFIX_PATH=/usr/local/ -DCMAKE_BUILD_TYPE=Release -DROCKSDB_BUILD_SHARED=OFF -DWITH_TESTS=OFF -DWITH_ALL_TESTS=OFF -DWITH_TOOLS=OFF -DWITH_BENCHMARK_TOOLS=OFF -DWITH_CORE_TOOLS=OFF -DWITH_TRACE_TOOLS=OFF -GNinja
 ninja -j 8
 cd ../..
 
@@ -19,13 +19,13 @@ git clone --recurse-submodules --depth 1 https://github.com/aws/aws-sdk-cpp
 cd aws-sdk-cpp/
 mkdir sdk_build
 cd sdk_build/
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=/usr/local/ -DCMAKE_INSTALL_PREFIX=/usr/local/ -DBUILD_ONLY="sqs"
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=/usr/local/ -DCMAKE_INSTALL_PREFIX=/usr/local/ -DBUILD_ONLY="sqs"
 make -j 8
 make install
 cd ../..
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -GNinja
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -GNinja
 ninja
 cd ..
